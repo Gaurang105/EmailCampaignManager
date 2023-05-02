44028dabcd2c4b7f4e101448f7693180079036f2
Gaurang105 <gujratigaurang@gmail.com>
2023-05-02 18:24:04 +0530

    comments updates

--- campaign/email.py
+++ campaign/email.py
@@ -2,7 +2,7 @@ from django.core.mail import EmailMultiAlternatives
 from django.utils.html import strip_tags
 
 def send_email(subject, to_email, html_content):
-    text_content = strip_tags(html_content)  # This will extract plain text from your HTML content
+    text_content = strip_tags(html_content)  # This will extract plain text from HTML content
     email = EmailMultiAlternatives(
         subject,
         text_content,
